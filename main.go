fa22024ce1d89619bde4db4566401db60f0636f5
Playaer <playaer80@gmail.com>
2016-10-31 18:48:55 +0300

    first draft

---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+	"net/http"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	PING string = "0"
+	ACCEPT string = "1"
+)
+
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
+
+var (
+	skno = syscall.NewLazyDLL("skno.dll")
+	openSknoDll = skno.NewProc("open")
+	closeSknoDll = skno.NewProc("close")
+	sendEventSknoDll = skno.NewProc("sendEvent")
+)
+
+func main() {
+	//err := initialize()
+	//if err != nil {
+	//	fmt.Println("Init:", err)
+	//	// block
+	//}
+	go startProxyServer()
+
+	testServer()
+
+	//testClient()
+
+}
+
+func initialize() error {
+	err := openSknoDll.Find()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	err = closeSknoDll.Find()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	err = sendEventSknoDll.Find()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	ret, _, err := openSknoDll.Call()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if ret != 1 {
+		str := fmt.Sprintf("Returned %d", ret)
+		return &errorString{str}
+	}
+
+	fmt.Println(ret)
+
+	return nil
+}
+
+func closeSkno() error {
+	ret, _, err := closeSknoDll.Call()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if ret != 1 {
+		str := fmt.Sprintf("Returned %d", ret)
+		return &errorString{str}
+	}
+
+	return nil
+}
+
+func sendDataSkno(eventType string, value int) error {
+	ret, _, err := sendEventSknoDll.Call(
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(eventType))),
+		uintptr(value),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if ret != 1 {
+		str := fmt.Sprintf("Returned %d", ret)
+		return &errorString{str}
+	}
+
+	return nil
+}
+
+func proxy(parentResp http.ResponseWriter, parentReq *http.Request) {
+	client := &http.Client{}
+
+	fmt.Println(parentReq.Method)
+	fmt.Println(parentReq.RequestURI)
+
+	clientReq, err := http.NewRequest(parentReq.Method, "http://daroo.by" + parentReq.RequestURI, parentReq.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	// set headers
+	h1 := parentReq.Header.Get("name")
+	clientReq.Header.Add("name", h1)
+
+	clientResp, err := client.Do(clientReq)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	respBody, err := ioutil.ReadAll(clientResp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	clientResp.Body.Close()
+	parentResp.Header().Set("Content-Type", clientResp.Header.Get("Content-Type"))
+	parentResp.Write(respBody)
+}
+
+func startProxyServer() {
+
+	http.HandleFunc("/", proxy)
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Server proxy started")
+}
+
+func testClient() {
+	fmt.Println("Client send request")
+	resp, err := http.Get("http://127.0.0.1:8888")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println("Client: ", string(body))
+}
+
+func testServer() {
+	http.HandleFunc("/test/*", hello)
+	err := http.ListenAndServe(":9999", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Server started")
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	postData := ""
+	if req.Method == "POST" {
+		postData = req.FormValue("bbb")
+	}
+	io.WriteString(
+		res,
+		`<doctype html>
+<html>
+	<head>
+		<title>Hello World</title>
+	</head>
+	<body>
+		Hello World! ` + postData + `
+	</body>
+</html>`,
+	)
+}
